95f7484dde1cd38c23ab81cee84cc23f99f278f5
Koxei <ciekawe320@gmail.com>
2025-01-21 01:58:33 +0100

    x

--- src/pages/TerminalPage.tsx
+++ src/pages/TerminalPage.tsx
@@ -1,11 +1,18 @@
+import AppOverlay from "@/components/layouts/AppOverlay";
+
 import { Terminal } from "@/components/Terminal/Terminal";
 
 const TerminalPage = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center p-4 bg-deep-sea-blue">
-      <Terminal />
-    </div>
-  );
+
+return (
+
+<AppOverlay title="CYBERPUNK TERMINAL">
+  <div className="min-h-screen flex items-center justify-center p-4 bg-deep-sea-blue">
+    <Terminal />
+  </div>
+</AppOverlay>
+);
+
 };
 
 export default TerminalPage;
