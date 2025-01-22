bd10253a3693667e1bd03bae2439bba11c3100a4
Koxei <ciekawe320@gmail.com>
2025-01-22 04:15:59 +0100

    again

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,12 +7,6 @@ const Login = () => {
 
   return (
     <div className="fixed inset-0">
-      {/* Background image with blur */}
-      <div 
-        className="absolute inset-0 bg-cover bg-center bg-no-repeat blur-xl brightness-50"
-        style={{ backgroundImage: "url('/lovable-uploads/73e97728-d0f0-4a4f-8e49-34667bc28380.png')" }}
-      />
-      {/* Content overlay */}
       <div className="relative z-10">
         <AuthModal 
           isOpen={true}
