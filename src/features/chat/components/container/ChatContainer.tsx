c2eabe6a58159f7d7cbd27055ea103c8111974ce
Koxei <ciekawe320@gmail.com>
2025-02-03 15:42:24 +0100

    lm

--- src/features/chat/components/container/ChatContainer.tsx
+++ src/features/chat/components/container/ChatContainer.tsx
@@ -23,6 +23,15 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
   return (
     <div className="relative z-30 flex flex-col h-full">
       <div className="flex flex-col h-full max-w-2xl mx-auto w-full border-2 border-red-500">
+        {/* Fixed header with transparent background */}
+        <div className="absolute top-0 left-0 right-0 z-50">
+          <div className="relative flex justify-between items-center p-4">
+            <div className="absolute inset-x-0 flex justify-center items-center">
+              <h1 className="text-xl font-bold">Chat</h1>
+            </div>
+          </div>
+        </div>
+        
         {/* Messages container with padding-top to account for fixed header */}
         <main className="flex-1 overflow-y-auto px-4 pb-4 pt-16">
           <ChatMessages 
